cb82a0dd11d34f47adf67271dc8847a9ddf8ee0b
nbert71 <nicobert2205@gmail.com>
2023-01-15 23:15:21 +0100

    💩 bon soon ça marche mais pas actuellement

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 require('dotenv').config()
-const path = require('path')
 
-const express = require('express')
+const { PrismaClient } = require('@prisma/client');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql');
+const typeDefs = require('./schema.graphql');
 
+const express = require('express');
 const app = express()
 const port = process.env.NODE_PORT
 
-app.use('/docs', express.static('docs'))
+const prisma = new PrismaClient();
+
+const types = buildSchema(typeDefs)
+const resolvers = {
+  Query: {
+    users: () => { return prisma.user.findMany()},
+    posts: () => { return prisma.post.findMany()},
+    profiles: () => { return prisma.profile.findMany()},
+  }
+};
+
+// const schema = makeExecutableSchema({ resolvers, types});
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello world !')
 })
+
+app.use('/api', graphqlHTTP({
+  schema: types,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.use('/docs', express.static('docs'))
 
 app.listen(port, () => {
   console.log(`The app is running on port ${port}`);
