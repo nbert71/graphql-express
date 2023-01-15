9733228fb0a886ee8f7cebfc057587a35fc2f203
nbert71 <nicobert2205@gmail.com>
2023-01-15 21:46:05 +0100

    add docs

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
+const path = require('path')
 
 const express = require('express')
 
 const app = express()
 const port = process.env.NODE_PORT
+
+app.use('/docs', express.static('docs'))
 
 app.get('/', (req, res) => {
   res.send('Hello World')
